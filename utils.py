0b3bcfea12374e09300ab8a5912ed259505478dc
VannshJani <141410835+VannshJani@users.noreply.github.com>
2024-03-14 23:33:41 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ if file_content is not None:
 
 # using LSTM
 class LSTM(nn.Module):
-    def __init__(self, block_size = 8, vocab_size = 65, emb_dim = 8, hidden_dims = [256, 256]):
+    def __init__(self, block_size = 8, vocab_size = 65, emb_dim = 8, hidden_dims = [1024, 1024]):
         super().__init__()
         self.emb = nn.Embedding(vocab_size, emb_dim)
         self.lstm = nn.LSTM(emb_dim, hidden_dims[0], num_layers=2, batch_first=True,bias = True)
@@ -70,4 +70,4 @@ file_id2 = "1Jg7SoBVLKtYbdAcWvRnPiq8KalxA9PYT"
 dest_path2 = "https://drive.google.com/file/d/1Jg7SoBVLKtYbdAcWvRnPiq8KalxA9PYT/view?usp=sharing"
 gdd.download_file_from_google_drive(file_id=file_id2, dest_path=dest_path2)
 lstm = LSTM()
-# lstm.load_state_dict(torch.load(dest_path2))
+lstm.load_state_dict(torch.load(dest_path2))
