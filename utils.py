8801f1acbd32e35e391b9d10e0def9101af006d7
VannshJani <141410835+VannshJani@users.noreply.github.com>
2024-03-14 23:44:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -67,7 +67,7 @@ mlp = NextChar()
 mlp.load_state_dict(torch.load(dest_path))
 
 file_id2 = "1Jg7SoBVLKtYbdAcWvRnPiq8KalxA9PYT"
-dest_path2 = "https://drive.google.com/file/d/1Jg7SoBVLKtYbdAcWvRnPiq8KalxA9PYT/view?usp=sharing"
+dest_path2 = "https://drive.google.com/file/d/1Jg7SoBVLKtYbdAcWvRnPiq8KalxA9PYT/view?usp=drive_link"
 gdd.download_file_from_google_drive(file_id=file_id2, dest_path=dest_path2)
 lstm = LSTM()
-# lstm.load_state_dict(torch.load(dest_path2))
+lstm.load_state_dict(torch.load(dest_path2))
