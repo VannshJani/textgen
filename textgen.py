58d22b0fa6d33f0f1adca77877705f2f26482527
VannshJani <141410835+VannshJani@users.noreply.github.com>
2024-03-14 19:42:02 +0530

    Update textgen.py

--- textgen.py
+++ textgen.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
+from utils import data
 
 
 st.title('Text Generation with MLPs and LSTM')
@@ -17,7 +18,7 @@ num_chars = st.slider('Number of characters to generate:', 1, 200, 20)
 st.sidebar.title('Model Details')
 model_name = st.sidebar.selectbox('Select model:', ['MLP', 'LSTM'])
 
-data = open('https://github.com/VannshJani/textgen/blob/main/input.txt', 'r').read()
+# data = open('https://github.com/VannshJani/textgen/blob/main/input.txt', 'r').read()
 unique_chars = list(set(''.join(data)))
 unique_chars.sort()
 vocab_dict = {i:ch for i, ch in enumerate(unique_chars)}
